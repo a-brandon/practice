fe373fe3c97a83efded79309aa049f66d233ef74
a-brandon <51302267+a-brandon@users.noreply.github.com>
2020-01-16 18:03:46 -0500

    Delete unnecessary code

--- leetcode/defanging_ip_address.py
+++ leetcode/defanging_ip_address.py
@@ -1,9 +1,3 @@
 class Solution:
     def defangIPaddr(self, address: str) -> str:
-        s = []
-        for character in address:
-            if character == '.':
-                s.append(character.replace(character, '[.]'))
-            else:
-                s.append(character)
-        return ''.join(s)
+        return address.replace('.', '[.]')
