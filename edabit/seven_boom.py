62e6892c882afe043acf67a4d00bd10acc0a4e0d
a-brandon <51302267+a-brandon@users.noreply.github.com>
2020-01-16 14:48:05 -0500

    Delete seven_boom.py

--- edabit/seven_boom.py
+++
@@ -1,6 +0,0 @@
-def seven_boom(lst):
-    str_list = [str(num) for num in lst]
-    boom_list = [int(digit) for num in str_list for digit in num if int(digit) == 7]
-    if boom_list:
-        return 'Boom!'
-    return 'there is no 7 in the list'
