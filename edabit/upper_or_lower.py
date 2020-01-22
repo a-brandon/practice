dc6da58f5e3322a7b2610407357437ed48accd67
realbrandonhere <realbrandonhere@gmail.com>
2020-01-22 16:22:46 -0500

    Return smallest number of steps to convert string to upper or lower

---
+++ edabit/upper_or_lower.py
@@ -0,0 +1,12 @@
+def steps_to_convert(txt: str) -> int:
+    upper_count = 0
+    lower_count = 0
+    for ch in txt:
+        if ch.islower():
+            lower_count += 1
+        else:
+            upper_count += 1
+    if upper_count > lower_count:
+        return lower_count
+    else:
+        return upper_count
