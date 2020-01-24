069c8ac0f4783586830e1cf724a9a973818e77bd
realbrandonhere <realbrandonhere@gmail.com>
2020-01-24 17:40:12 -0500

    Add type hints

--- edabit/last_digit_ultimate.py
+++ edabit/last_digit_ultimate.py
@@ -1,4 +1,4 @@
-def last_dig(a, b, c):
+def last_dig(a: int, b: int, c: int) -> bool:
     a, b, c = str(a), str(b), str(c)
     result = [int((num[-1])) for num in (a, b, c)]
     product = str(result[0] * result[1])
