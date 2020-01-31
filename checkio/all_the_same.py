b48326134628c7fdeba476b2d2ef133d406a9c8f
a-brandon <51302267+a-brandon@users.noreply.github.com>
2020-01-31 15:16:56 -0500

    Change beginning of for loop to second element

--- checkio/all_the_same.py
+++ checkio/all_the_same.py
@@ -2,7 +2,7 @@ from typing import List, Any
 
 
 def all_the_same(elements: List[Any]) -> bool:
-    for elem in elements:
+    for elem in elements[1:]:
         if elem != elements[0]:
             return False
     return True
