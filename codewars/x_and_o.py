cead834ecd40ca5857d9afd52bf4c3f11f9c44d8
realbrandonhere <realbrandonhere@gmail.com>
2020-02-03 13:51:53 -0500

    Exes and Ohs

---
+++ codewars/x_and_o.py
@@ -0,0 +1,5 @@
+def xo(s):
+    """Check to see if a string has the same amount of 'x's and 'o's. 
+    The method must return a boolean and be case 
+    insensitive. The string can contain any char. """
+    return s.lower().count('x') == s.lower().count('o')
