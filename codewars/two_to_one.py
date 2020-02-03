56d9bb60a1434637d99836f5129101fc4d117f7f
realbrandonhere <realbrandonhere@gmail.com>
2020-02-03 14:01:51 -0500

    Two to One

---
+++ codewars/two_to_one.py
@@ -0,0 +1,4 @@
+def longest(s1, s2):
+    """Take 2 strings s1 and s2 including only letters from ato z. 
+    Return a new sorted string, the longest possible, containing distinct letters."""
+    return ''.join(sorted(set(s1 + s2)))
