889c8faf151070a30bfa5b930348de7065c1d36f
a-brandon <51302267+a-brandon@users.noreply.github.com>
2020-02-03 13:52:59 -0500

    Add type hints

--- codewars/x_and_o.py
+++ codewars/x_and_o.py
@@ -1,4 +1,4 @@
-def xo(s):
+def xo(s: str) -> bool:
     """Check to see if a string has the same amount of 'x's and 'o's. 
     The method must return a boolean and be case 
     insensitive. The string can contain any char. """
