86bd9d906ac254ba9ca57045c527f5c07caefdfe
a-brandon <51302267+a-brandon@users.noreply.github.com>
2020-02-03 14:02:48 -0500

    Edit typo

--- codewars/two_to_one.py
+++ codewars/two_to_one.py
@@ -1,4 +1,4 @@
 def longest(s1, s2):
-    """Take 2 strings s1 and s2 including only letters from ato z. 
+    """Take 2 strings s1 and s2 including only letters from a to z. 
     Return a new sorted string, the longest possible, containing distinct letters."""
     return ''.join(sorted(set(s1 + s2)))
