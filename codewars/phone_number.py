76fc2f5d68e8e3efe010d4cb833aeb482a13c70c
realbrandonhere <realbrandonhere@gmail.com>
2020-02-04 12:27:48 -0500

    Phone number

---
+++ codewars/phone_number.py
@@ -0,0 +1,7 @@
+def create_phone_number(n):
+    """Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those 
+    numbers in the form of a phone number. """
+    number = ''
+    for num in n:
+        number += str(num)
+    return f'({number[0:3]}) {number[3:6]}-{number[6:]}'
