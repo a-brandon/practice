58a63e9d628790d5b0db36296367512bf961cd5c
a-brandon <51302267+a-brandon@users.noreply.github.com>
2020-02-04 12:36:11 -0500

    Refactor (list comprehension)

--- checkio/non_unique.py
+++ checkio/non_unique.py
@@ -1,6 +1,2 @@
 def checkio(data: list) -> list:
-    non_unique = []
-    for num in data:
-        if data.count(num) >= 2:
-            non_unique.append(num)
-    return non_unique
+    return [num for num in data if data.count(num) >= 2]
