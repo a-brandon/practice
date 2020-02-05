3a28d94e182d8e2dacc848e6799434fe5c97b97d
realbrandonhere <realbrandonhere@gmail.com>
2020-02-04 20:00:31 -0500

    Kebabize

---
+++ codewars/kebabize.py
@@ -0,0 +1,20 @@
+def kebabize(sentence: str) -> str:
+    """
+    Kebabizes a string.
+
+    Turns a camel-cased string into a kebab-cased string.
+    
+    Args:
+        sentence: A camel-cased string.
+    
+    Returns:
+        A kebab-cased string.
+        
+    """
+    output = ''
+    for ch in sentence:
+        if ch.isupper():
+            output += "-" + ch
+        elif not ch.isdigit():
+            output += ch
+    return output.lower().strip('-')
