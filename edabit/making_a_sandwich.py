e0b5aa0f8015795fae9d10ac2beb005e943772ff
realbrandonhere <realbrandonhere@gmail.com>
2020-02-17 16:17:04 -0500

    Making A Sandwich

--- edabit/making_a_sandwich.py
+++ edabit/making_a_sandwich.py
@@ -1,4 +1,14 @@
-def make_sandwich(i, f):
+def make_sandwich(i: list[str], f: str) -> list:
+    """Makes a sandwich.
+    
+    Creates a sandwich based off the ingredients in the list.
+    If the ingredient == f, then "bread" gets placed around the ingredient.
+    
+    Parameters:
+        i: Ingredients
+        f: An ingredient to place "bread" around.
+    Returns:
+        A sandwich."""
     sandwich = ''
     for ingredient in i:
         if ingredient == f:
