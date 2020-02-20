233856d56ff4426ae84459a87f011ac381c780f0
realbrandonhere <realbrandonhere@gmail.com>
2020-02-20 17:19:59 -0500

    Exercises

---
+++ structures_and_algorithms/reinforcement/r1.10.py
@@ -0,0 +1,7 @@
+def get_range():
+    """R-1.10
+
+    What parameters should be sent to the range constructor, to produce a
+    range with values 8, 6, 4, 2, 0, −2, −4, −6, −8?"""
+    for i in range(8, -10, -2):
+        print(i)
---
+++ structures_and_algorithms/reinforcement/r1.11.py
@@ -0,0 +1,6 @@
+def show_comp():
+    """R-1.11
+    
+    Demonstrate how to use Python’s list comprehension syntax to produce
+the list [1, 2, 4, 8, 16, 32, 64, 128, 256]."""
+    return [2 ** i for i in range(9)]
