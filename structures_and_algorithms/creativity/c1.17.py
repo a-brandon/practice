bd5dd0218d9629d481c0e038029e9992d7476d5e
realbrandonhere <realbrandonhere@gmail.com>
2020-02-21 16:31:24 -0500

    Exercises

---
+++ structures_and_algorithms/creativity/c1.17.py
@@ -0,0 +1,13 @@
+"""C1-17
+
+Had we implemented the scale function (page 25) as follows,
+does it work properly?
+"""
+
+
+def scale(data, factor):
+    for val in data:
+        val *= factor
+
+# Yes
+
