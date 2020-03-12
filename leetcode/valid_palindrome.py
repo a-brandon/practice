3085ce3e4d7ddac9647d4f8f5a6fbcdc1cf549aa
realbrandonhere <realbrandonhere@gmail.com>
2020-03-12 15:15:51 -0400

    Valid Palindrome

---
+++ leetcode/valid_palindrome.py
@@ -0,0 +1,10 @@
+from string import punctuation
+
+
+def find_palindrome(s):
+    if not s:
+        return True
+    for c in punctuation:
+        s = s.replace(c, ' ').lower()
+    s = ''.join(s.split())
+    return True if s == s[::-1] else False
