6833273cae1483bd04247d88f72082ab796cd9c9
a-brandon <51302267+a-brandon@users.noreply.github.com>
2020-03-15 01:20:28 -0400

    Update valid_palindrome.py

--- leetcode/valid_palindrome.py
+++ leetcode/valid_palindrome.py
@@ -4,6 +4,7 @@ from string import punctuation
 def find_palindrome(s):
     if not s:
         return True
+
     for c in punctuation:
         s = s.replace(c, ' ').lower()
     s = ''.join(s.split())
