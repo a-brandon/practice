49600407279ea4128317d92d5b540caba7fb2a38
a-brandon <51302267+a-brandon@users.noreply.github.com>
2020-03-20 17:20:03 -0400

    Change function name, remove unnecessary lines

--- edabit/moran_numbers.py
+++ edabit/moran_numbers.py
@@ -5,7 +5,7 @@ def is_prime(num):
     return True
 
 
-def find_harshad(num):
+def is_harshad(num):
     digits = sum(int(i) for i in str(num))
     if num % digits == 0:
         return 'H'
@@ -16,5 +16,4 @@ def moran(n):
     digits = sum(int(i) for i in str(n))
     if n % digits == 0 and is_prime(n // digits):
         return 'M'
-    else:
-        return find_harshad(n)
+    return is_harshad(n)
