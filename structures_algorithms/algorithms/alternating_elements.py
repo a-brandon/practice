ea6638e128381caeb550de1cdd72d886c375a2dc
a-brandon <51302267+a-brandon@users.noreply.github.com>
2020-03-24 19:30:31 -0400

    Update alternating_elements.py

--- structures_algorithms/algorithms/alternating_elements.py
+++ structures_algorithms/algorithms/alternating_elements.py
@@ -1,5 +1,12 @@
-s = 'ABABAB'
-for i, _ in enumerate(s[:-2]): # Doing it this way we can check if the current element matches the letter two elements ahead of it, without exceeding index range.
-  if s[i] != s[i + 2]: # Check if current element isn't equal to the element two index positions head.
-    return False # Return False as soon as the condition isn't met.
-
+def is_alternating(s: str) -> bool:
+    """
+    Checks if a string has alternating characters.
+    
+    Uses the enumerate function, and slices the string up
+    to the second-to-last character, to prevent an index error on iteration.
+    Iterates through the string, and checks if a character is equal to
+    the character two index positions ahead of it.
+    """
+    for i, _ in enumerate(s[:-2]):
+        if s[i] != s[i + 2]:
+            return False
