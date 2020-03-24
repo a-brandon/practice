979490be408a7b4a674c64104fecf51e7242cf3a
realbrandonhere <realbrandonhere@gmail.com>
2020-03-24 17:14:26 -0400

    Rot13

---
+++ codewars/rot13.py
@@ -0,0 +1,17 @@
+from string import ascii_lowercase
+
+
+def rot13(message):
+    alpha = ascii_lowercase + ascii_lowercase
+    print(alpha)
+    rot = ''
+    for c in message:
+        if c.isalpha() and c.isupper():
+            idx = alpha.index(c.lower())
+            rot += alpha[idx + 13].upper()
+        elif c.isalpha() and c.islower():
+            idx = alpha.index(c)
+            rot += alpha[idx + 13]
+        else:
+            rot += c
+    return rot
