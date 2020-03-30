2fbfb506db847a3004d2cd8bf3ca24b79fd92f8c
realbrandonhere <realbrandonhere@gmail.com>
2020-03-30 17:14:57 -0400

    Self Describing Numbers

--- edabit/self_describing_numbers.py
+++ edabit/self_describing_numbers.py
@@ -3,4 +3,4 @@ def is_self_describing(num: int) -> bool:
         return False
     num = str(num)
     pairs = [num[i * 2: i * 2 + 2] for i in range(len(num) // 2)]
-    return all(len(int(pair[0]) * pair[1]) == num.count(pair[1]) for pair in pairs)
+    return all(len(int(x) * y) == num.count(y) for x, y in pairs)
