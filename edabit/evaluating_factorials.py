69af2c69286304cb6255ab515d3e5abbcfc9e178
realbrandonhere <realbrandonhere@gmail.com>
2020-04-08 15:34:22 -0400

    Evaluating Factorials

--- edabit/evaluating_factorials.py
+++ edabit/evaluating_factorials.py
@@ -1,12 +1,16 @@
+def multiply(nums) -> int:
+    res = 1
+    for x in nums:
+        res *= x
+    return res
+
+
 def eval_factorial(lst):
     f = []
     for fact in lst:
         if fact == '0!' or fact == '1!':
             f.append(1)
             continue
-        fact = [i for i in range(1, int(fact[0:-1]) + 1)]
-        res = 1
-        for x in fact:
-            res *= x
-        f.append(res)
+        fact = multiply(i for i in range(1, int(fact[0:-1]) + 1))
+        f.append(fact)
     return sum(f)
