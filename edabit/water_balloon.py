56ed5e3837cf0180830531d9b2dfd86db839050b
realbrandonhere <realbrandonhere@gmail.com>
2020-04-08 19:19:20 -0400

    Water Balloon

---
+++ edabit/water_balloon.py
@@ -0,0 +1,8 @@
+def pop(state):
+    if len(state) == 1 and state == [0]:
+        return state
+    balloon = state
+    pop_idx = next(i for i in state if i)
+    balloon[1:pop_idx] = [i for i in range(1, pop_idx)]
+    balloon[pop_idx + 1:-1] = [i for i in range(pop_idx - 1, 0, -1)]
+    return balloon
