aa86f0434c3df4ae15aa363539c2b564f70c9147
realbrandonhere <realbrandonhere@gmail.com>
2020-04-20 18:51:38 -0400

    Advanced List Sort

--- edabit/advanced_list_sort.py
+++ edabit/advanced_list_sort.py
@@ -1,7 +1,2 @@
 def advanced_sort(lst):
-    dupes = [[x] * lst.count(x) for x in lst]
-    unique = []
-    for el in dupes:
-        if el not in unique:
-            unique.append(el)
-    return unique
+    return sorted([[x] * lst.count(x) for x in set(lst)], key=lambda x: lst.index(x[0]))
