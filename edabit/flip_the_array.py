22b759f05ba89674fa918fccb88cbeeed6d05b05
realbrandonhere <realbrandonhere@gmail.com>
2020-04-23 15:43:26 -0400

    Refactor

--- edabit/flip_the_array.py
+++ edabit/flip_the_array.py
@@ -1,4 +1,2 @@
 def flip_list(lst):
-    if lst:
-        return [[i] for i in lst if type(lst[0]) == int] or sum(lst, [])
-    return []
+    return [[i] for i in lst if isinstance(lst[0], int)] or sum(lst, []) or []
