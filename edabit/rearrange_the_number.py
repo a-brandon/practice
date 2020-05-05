06ebbd25b6a63352106365eac714a4b4d98887db
realbrandonhere <realbrandonhere@gmail.com>
2020-05-05 13:15:00 -0400

    Refactor

--- edabit/rearrange_the_number.py
+++ edabit/rearrange_the_number.py
@@ -1,6 +1,2 @@
 def rearranged_difference(num):
-    min_list = sorted(i for i in str(num))
-    min_number = int(''.join([i for i in min_list]))
-    max_list = list(reversed(min_list))
-    max_number = int(''.join(max_list))
-    return max_number - min_number
+    return int(''.join(sorted(str(num), reverse=True))) - int(''.join(sorted(str(num))))
