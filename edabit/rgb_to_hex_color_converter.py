8c8fab1d236a923f088a75b8357c28b6d4f6809a
realbrandonhere <realbrandonhere@gmail.com>
2020-05-08 16:43:35 -0400

    RGB To Hex Color Converter

--- edabit/rgb_to_hex_color_converter.py
+++ edabit/rgb_to_hex_color_converter.py
@@ -1,5 +1,5 @@
 def rgb_to_hex(col):
-    s = col.replace('rgb(', '').replace(')', '').split(',')
-    nums = list(map(int, s))
+    colors = col.replace('rgb(', '').replace(')', '').split(',')
+    nums = list(map(int, colors))
     conversion = ''.join('00' if n == 0 else hex(n)[2:] for n in nums)
     return '#' + conversion
