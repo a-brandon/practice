dcef0ca42dbf268be42c905b88148ec7072a0301
realbrandonhere <realbrandonhere@gmail.com>
2020-05-14 15:41:14 -0400

    The Happy Algorithm

--- edabit/the_happy_function.py
+++ edabit/the_happy_function.py
@@ -1,6 +1,6 @@
 def happy_algorithm(num):
     if num == 1:
-        return 'HAAPY 1'
+        return 1
 
     happy_nums = set()
     happy_nums.add(num)
@@ -10,7 +10,7 @@ def happy_algorithm(num):
         total = sum(x ** 2 for x in digits)
         num = total
         if num in happy_nums:
-            return 'SAD ' + str(count + 1)
+            return f'SAD {str(count + 1)}'
         happy_nums.add(num)
         count += 1
-    return 'HAPPY ' + str(count)
+    return f'HAPPY {str(count)}'
