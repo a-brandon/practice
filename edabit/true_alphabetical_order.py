086ed6333452ac0d08e987b56ae71a455b9ad7d4
realbrandonhere <realbrandonhere@gmail.com>
2020-05-14 13:33:03 -0400

    True Alphabetical Order

--- edabit/true_alphabetical_order.py
+++ edabit/true_alphabetical_order.py
@@ -1,3 +1,3 @@
 def true_alphabetic(txt):
-    t = sorted(txt.replace(' ', ''))[::-1]
-    return ''.join(t.pop() if c.isalpha() else ' ' for c in txt)
+    l = sorted(txt.replace(' ', ''))[::-1]
+    return ''.join(l.pop() if c.isalpha() else ' ' for c in txt)
