af514a6f73e2982556ebc27392699d5b261e525b
realbrandonhere <realbrandonhere@gmail.com>
2020-05-15 14:13:26 -0400

    Kaprekars Constant

--- edabit/kaprekars_constant.py
+++ edabit/kaprekars_constant.py
@@ -1,6 +1,6 @@
 def kaprekar(num):
     count = 0
-    while num != 6174:
+    while num != 6174 and len(set(str(num))) > 1:
         asc = int(''.join(sorted(str(num).zfill(4))))
         desc = int(''.join(sorted(str(num).zfill(4), reverse=True)))
         num = max(asc, desc) - min(asc, desc)
