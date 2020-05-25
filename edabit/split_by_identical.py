5ad6bd0b370b3a2a157eeb4aca283c90cd9c90d9
realbrandonhere <realbrandonhere@gmail.com>
2020-05-25 15:07:34 -0400

    Refactor

--- edabit/split_by_identical.py
+++ edabit/split_by_identical.py
@@ -1,9 +1,10 @@
 def split_groups(txt):
-    output = ''
-    for i in range(len(txt) - 1):
-        output += txt[i]
-        if txt[i + 1] != txt[i]:
-            output += ','
-    missing_el = output + txt[-1]
-    sep_elements = missing_el.split(',')
-    return sep_elements
+    s, clusters = txt[0], []
+
+    for c in txt[1:]:
+        if c != s[-1]:
+            clusters.append(s)
+            s = ''
+        s += c
+
+    return clusters + [s]
