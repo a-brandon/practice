22e036d64132507bc1aec0f7b2a96b3ef143a569
realbrandonhere <realbrandonhere@gmail.com>
2020-05-25 15:44:56 -0400

    Refactor

--- edabit/anonymous_name.py
+++ edabit/anonymous_name.py
@@ -1,6 +1,6 @@
 def is_correct_aliases(names, aliases):
-    for name, alias in zip(names, aliases):
-        letter = name[0]
-        if alias.count(letter) != 2:
+    for a, b in zip(names, aliases):
+        anons = b.split()
+        if not (anons[0].startswith(a[0]) and anons[1].startswith(a[0])):
             return False
     return True
