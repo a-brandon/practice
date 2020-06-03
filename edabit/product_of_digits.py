a0208cc22c0c6b7f43f7ab6ce328f08c2796b7c0
a-brandon <realbrandonhere@gmail.com>
2020-06-03 15:53:20 -0400

    Refactor

--- edabit/product_of_digits.py
+++ edabit/product_of_digits.py
@@ -1,13 +1,8 @@
 def sum_dig_prod(*args):
-    number = [int(i) for i in str(sum(args))]
-    prod = 1
-    count = 0
-    while len(number) > 1:
-        for x in number:
-            prod *= x
-            count += 1
-        if count == len(number):
-            number = [int(i) for i in str(prod)]
-            prod = 1
-            count = 0
-    return number[0]
+    total = str(sum(args))
+    while len(total) > 1:
+        x = 1
+        for n in total:
+            x *= int(n)
+        total = str(x)
+    return int(total)
