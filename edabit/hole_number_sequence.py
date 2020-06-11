e9dace9db36a6570e1eb3a713f11e600898191a3
a-brandon <realbrandonhere@gmail.com>
2020-06-11 14:20:51 -0400

    Refactor

--- edabit/hole_number_sequence.py
+++ edabit/hole_number_sequence.py
@@ -1,9 +1,9 @@
-def sum_of_holes(N: int) -> int:
-    holes = {0: 1, 4: 1, 6: 1, 8: 2, 9: 1}
-    total = []
-    for num in range(1, N + 1):
-        arr = list(str(num))
-        for x in arr:
-            if int(x) in holes:
-                total.append(holes[int(x)])
-    return sum(total)
+def sum_of_holes(n):
+    d = {0: 1, 4: 1, 6: 1, 8: 2, 9: 1}
+    c = 0
+    for i in range(1, n + 1):
+        if len(str(i)) == 1:
+            c += d.get(i, 0)
+        else:
+            c += sum(d.get(int(x), 0) for x in str(i))
+    return c
