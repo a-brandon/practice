6e7fb913365d9e98f0a17a251097f1cc3e1d37c3
a-brandon <realbrandonhere@gmail.com>
2020-06-11 13:09:09 -0400

    Holey Sort

--- edabit/holey_sort.py
+++ edabit/holey_sort.py
@@ -1,6 +1,3 @@
 def holey_sort(lst):
     holes = {4: 1, 6: 1, 9: 1, 0: 1, 8: 2}
     return sorted(lst, key=lambda n: sum(holes.get(int(x), 0) for x in str(n)))
-
-
-print(holey_sort([8, 121, 41, 66]))
