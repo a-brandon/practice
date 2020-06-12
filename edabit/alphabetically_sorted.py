3ba3bee0a90af6502375c3ffb37f6f9970dd08fe
a-brandon <realbrandonhere@gmail.com>
2020-06-12 07:25:04 -0400

    Refactor

--- edabit/alphabetically_sorted.py
+++ edabit/alphabetically_sorted.py
@@ -1,17 +1,10 @@
-from string import punctuation
-
-
 def is_alphabetically_sorted(sentence):
-    # Needs a lot of refactoring. Bad bruteforce solution
-    new_sentence = ''
-    for ch in sentence:
-        if ch not in punctuation:
-            new_sentence += ch.lower()
-    split_sentence = new_sentence.split()
-    count = 0
-    for chars in split_sentence:
-        joined_word = [''.join(sorted(chars))]
-        for word in joined_word:
-            if word in split_sentence and len(word) >= 3:
-                count += 1
-    return bool(count)
+    s = ''.join(ch for ch in sentence if ch.isalpha() or ch.isspace()).lower().split()
+    for word in s:
+        if len(word) >= 3 and list(word) == sorted(word):
+            return True
+    return False
+
+
+
+print(is_alphabetically_sorted("Beth dislikes eating starfruit but enjoys cherries."))
