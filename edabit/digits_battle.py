90488857f8cfcee2f3ba40ac901caf0a54557ea3
a-brandon <realbrandonhere@gmail.com>
2020-06-15 13:36:41 -0400

    Refactor

--- edabit/digits_battle.py
+++ edabit/digits_battle.py
@@ -1,12 +1,4 @@
 def battle_outcome(num):
-    n = list(map(int, str(num)))
-    digits = []
-    for i in range(len(n) // 2):
-        pairs = n[i * 2:i * 2 + 2]
-        if pairs[0] == pairs[1]:
-            continue
-        else:
-            digits.append(max(pairs))
-    if len(n) % 2 != 0:
-        digits = digits + [n[-1]]
-    return int(''.join(map(str, digits[:])))
+    s = str(num)
+    res = [max(s[i: i + 2]) for i in range(0, len(s), 2) if len(set(s[i: i + 2])) > 1]
+    return int(''.join(res + [(s[-1])])) if len(s) % 2 else int(''.join(res))
