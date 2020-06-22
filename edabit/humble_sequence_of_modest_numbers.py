f3234af0771a1844add365a7a2a5df26338ca8ab
a-brandon <realbrandonhere@gmail.com>
2020-06-22 14:00:27 -0400

    Humble Sequence Of Modest Numbers

--- edabit/humble_sequence_of_modest_numbers.py
+++ edabit/humble_sequence_of_modest_numbers.py
@@ -1,7 +1,9 @@
 def is_modest(num):
     n, count = str(num), 0
+
     for i in range(len(n[:-1])):
         l, r = int(n[:i + 1]), int(n[i + 1:])
-        if r != 0:
-            count += 1 if num % r == l else 0
+        if r != 0 and num % r == l:
+            count += 1
+
     return count >= 1
