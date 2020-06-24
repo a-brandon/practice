6fbeba13e62f58cd3a31fdb59e0c5d76afe243ea
a-brandon <realbrandonhere@gmail.com>
2020-06-24 14:38:21 -0400

    Refactor

--- edabit/balanced_words.py
+++ edabit/balanced_words.py
@@ -1,15 +1,12 @@
-from string import ascii_lowercase
-
-
 def balanced_word(word):
-    if word == word[::-1]:
-        return True
-    vals = {letter: i for i, letter in enumerate(ascii_lowercase, start=1)}
-    if len(word) % 2 == 0:
-        first_half = sum([vals[c] for c in list(word[:len(word) // 2])])
-        second_half = sum([vals[c] for c in list(word[len(word) // 2:])])
-        if first_half == second_half:
-            return True
-    odd_first = sum([vals[c] for c in list(word[:len(word) // 2])])
-    odd_second = sum([vals[c] for c in list(word[len(word) // 2:][1:])])
-    return True if odd_first == odd_second else False
+    mid = len(word) // 2
+    l, r = word[:mid], word[mid:]
+    lc, rc = 0, 0
+    if len(word) % 2:
+        r = word[mid + 1:]
+    i = 0
+    while i < len(l):
+        lc += ord(l[i]) - 96
+        rc += ord(r[i]) - 96
+        i += 1
+    return lc == rc
