534f681d2e4d9958c1cee5e0074e6528985dd8ab
a-brandon <realbrandonhere@gmail.com>
2020-06-24 14:40:34 -0400

    Balanced Words Refactor

--- edabit/balanced_words.py
+++ edabit/balanced_words.py
@@ -1,12 +1,14 @@
 def balanced_word(word):
     mid = len(word) // 2
     l, r = word[:mid], word[mid:]
-    lc, rc = 0, 0
     if len(word) % 2:
         r = word[mid + 1:]
+
+    lc, rc = 0, 0
     i = 0
     while i < len(l):
         lc += ord(l[i]) - 96
         rc += ord(r[i]) - 96
         i += 1
+
     return lc == rc
