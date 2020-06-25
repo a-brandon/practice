2fee4270785a225b38bcd96c5765b0ac16ec14ac
a-brandon <realbrandonhere@gmail.com>
2020-06-25 12:38:28 -0400

    Every Value Needs Some Testing

---
+++ edabit/every_value_needs_some_testing.py
@@ -0,0 +1,5 @@
+def every_some(test, val, a, b, c, d, e):
+    var = (a, b, c, d, e)
+    if val == 'everybody':
+        return all(eval('{} {}'.format(x, test)) for x in var)
+    return any(eval('{} {}'.format(x, test)) for x in var)
