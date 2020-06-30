bce0c9f0cf3f769a6cc4729b41c8ec068dd5c2fa
a-brandon <realbrandonhere@gmail.com>
2020-06-30 14:58:52 -0400

    Refactor

--- edabit/first_letter_shift.py
+++ edabit/first_letter_shift.py
@@ -1,8 +1,8 @@
 def shift_sentence(txt):
-    words = txt.split()
-    shifts = []
-    for i, curr_word in enumerate(words):
-        prev, curr_letters = words[i - 1], list(curr_word)
-        curr_letters[0] = prev[0]
-        shifts.append(''.join(curr_letters))
-    return ' '.join(shifts)
+    s = txt.split()
+    for i, w in enumerate(s):
+        if w == s[0]:
+            s[i] = s[-1][0] + w[1:]
+        else:
+            s[i] = txt.split()[i - 1][0] + w[1:]
+    return ' '.join(s)
