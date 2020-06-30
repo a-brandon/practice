ffa69ef347b6fd391cce7092335d7cc17daad4a3
a-brandon <realbrandonhere@gmail.com>
2020-06-30 14:28:46 -0400

    Refactor

--- edabit/longest_abecedarion_word.py
+++ edabit/longest_abecedarion_word.py
@@ -1,12 +1,7 @@
-from string import ascii_lowercase
-
-
 def longest_abecedarian(lst):
-    alpha = ascii_lowercase
-    abecs = {}
-    for word in lst:
-        letters = [alpha.index(x) for x in word]
-        if sorted(letters) == letters:
-            abecs[word] = len(word)
-    longest_abecs = [k for k, v in abecs.items() if v == max(abecs.values())]
-    return longest_abecs[0] if longest_abecs else ''
+    longest_word = ''
+    for w in lst:
+        alpha = [ord(c) - 96 for c in w]
+        if alpha == sorted(alpha) and len(w) > len(longest_word):
+            longest_word = w
+    return longest_word
