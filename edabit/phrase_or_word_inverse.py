0982844696f206eb8086d66a388397fa8ad55dee
a-brandon <realbrandonhere@gmail.com>
2020-07-31 08:39:49 -0400

    Phrase Or Word Inverse

---
+++ edabit/phrase_or_word_inverse.py
@@ -0,0 +1,4 @@
+def inverter(txt, type):
+    conversion = {'P': ' '.join(txt.split()[::-1]).capitalize(),
+                  'W': ' '.join([w[::-1] for w in txt.split()]).capitalize()}
+    return conversion[type]
