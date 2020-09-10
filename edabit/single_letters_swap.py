ab5416b7a168610c053a0d76cfafcf2891a00f35
a-brandon <realbrandonhere@gmail.com>
2020-09-10 17:45:21 -0400

    Single Letters Swap

--- edabit/single_letters_swap.py
+++ edabit/single_letters_swap.py
@@ -1,14 +1,15 @@
 def validate_swaps(lst, txt):
-    swaps = []
-    for word in lst:
-        if len(word) > len(txt) or set(word) != set(txt):
-            swaps.append(False)
-            continue
-        counter, letters = 0, list(word)
-        for i, c in enumerate(word[:]):
-            if c != txt[i]:
-                c_idx = txt.index(c)
-                letters[c_idx] = txt[c_idx]
-                counter += 1
-        swaps.append(True if counter == 2 else False)
-    return swaps
+    bools = []
+
+    for w in lst:
+        swaps = []
+
+        for i, c in enumerate(w):
+            if c not in txt or len(w) != len(txt):
+                break
+            if w[i] != txt[i]:
+                swaps.append(c)
+
+        bools.append(True if len(swaps) == 2 else False)
+
+    return bools
