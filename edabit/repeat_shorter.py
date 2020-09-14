322d2dafd98f6eef604b71c2bc1cd89b89f87f35
a-brandon <realbrandonhere@gmail.com>
2020-09-14 17:23:16 -0400

    Repeat Shorter

--- edabit/repeat_shorter.py
+++ edabit/repeat_shorter.py
@@ -1,15 +1,3 @@
 def lengthen(s1, s2):
-    if len(s1) > len(s2):
-        output = ''
-        while True:
-            for char in s2:
-                output += char
-                if len(output) == len(s1):
-                    return output
-    else:
-        output = ''
-        while True:
-            for char in s1:
-                output += char
-                if len(output) == len(s2):
-                    return output
+    n = len(max(s1, s2, key=len))
+    return (n * min(s1, s2, key=len))[:n]
