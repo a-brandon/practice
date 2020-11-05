436e4c7a8149d027b5211ef1bc8870beb031fe5f
a-brandon <realbrandonhere@gmail.com>
2020-11-04 18:43:59 -0800

    Changes

--- codewars/replace_alphabet_position.py
+++ codewars/replace_alphabet_position.py
@@ -1,6 +1,2 @@
-from string import ascii_lowercase
-
-
 def alphabet_position(text):
-    alphabet = {letter: i for i, letter in enumerate(ascii_lowercase, start=1)}
-    return ' '.join([str(alphabet[letter]) for letter in text.lower() if letter.isalpha()])
+    return ' '.join(str(ord(c.lower()) - 96) for c in text if c.isalpha())
