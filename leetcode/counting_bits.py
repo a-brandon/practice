0eb55fbfc14bdeddd49758fd5442e76153284cd4
a-brandon <realbrandonhere@gmail.com>
2020-12-07 09:01:03 -0800

    Refactor

--- leetcode/counting_bits.py
+++ leetcode/counting_bits.py
@@ -1,9 +1,3 @@
 class Solution:
     def countBits(self, num: int) -> List[int]:
-        on_bits = [0]
-
-        for i in range(1, num + 1):
-            b = bin(i)[2:].replace('0', '')
-            on_bits.append(0 + len(b))
-
-        return on_bits
+        return [len(bin(i)[2:].replace('0', '')) for i in range(0, num + 1)]
