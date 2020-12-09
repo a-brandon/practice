9c10a7ffe7ea64745d7cde4bab66c9b5505cc9f0
a-brandon <realbrandonhere@gmail.com>
2020-12-09 08:35:45 -0800

    Solved Problems

--- codewars/detect_panagram.py
+++ codewars/detect_panagram.py
@@ -1,24 +1,5 @@
-from string import ascii_lowercase
+import string
 
 
-def is_pangram(s: str) -> bool:
-    """
-    Detects panagrams.
-
-    Checks if every letter in the string contains a letter in the alphabet
-    at least once.
-
-    Args:
-        s: A string.
-
-    Returns:
-        True or False.
-    """
-    count = 0
-    for letter in s.lower():
-        if letter in ascii_lowercase:
-            count += 1
-            if count == len(ascii_lowercase):
-                return True
-    return False
-
+def is_pangram(s):
+    return all(c in s.lower() for c in string.ascii_lowercase)
