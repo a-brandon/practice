c210668a853fdc60b2e079234290bda01669be0c
a-brandon <realbrandonhere@gmail.com>
2020-12-28 10:20:12 -0800

    Solved Problems

--- codewars/max_min_arrays.py
+++ codewars/max_min_arrays.py
@@ -1,1 +1,9 @@
+def solve(arr):
+    sorted_arr = sorted(arr)
+    result = []
 
+    while len(sorted_arr) > 1:
+        result.append(sorted_arr.pop())
+        sorted_arr = sorted_arr[::-1]
+
+    return result + sorted_arr
