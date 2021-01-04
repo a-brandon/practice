892a21f9f1c28f859ad1d2ef14016b807af9c3e2
a-brandon <realbrandonhere@gmail.com>
2021-01-04 10:11:47 -0800

    Solved Problems

---
+++ edabit/sentence_searcher.py
@@ -0,0 +1,7 @@
+def sentence_searcher(txt, word):
+    if word.lower() not in txt.lower():
+        return ''
+
+    for s in txt.lower().split('.'):
+        if word.lower() in s:
+            return (s.strip() + '.').capitalize()
