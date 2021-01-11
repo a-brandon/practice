0a7ddd8fb1e1fb338084f37692343ca84b50260d
a-brandon <realbrandonhere@gmail.com>
2021-01-11 12:13:39 -0800

    Solved Problems

---
+++ edabit/closing_in_sum.py
@@ -0,0 +1,9 @@
+def closing_in_sum(n):
+    nums = list(str(n))
+    combined_nums = []
+
+    while len(nums) > 1:
+        l, r = nums.pop(0), nums.pop()
+        combined_nums.append(int(l + r))
+
+    return sum(combined_nums) + int(nums[0]) if nums else sum(combined_nums)
