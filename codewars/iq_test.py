3e5de5089d02732a0b62395c8565343e5ea6a28b
a-brandon <realbrandonhere@gmail.com>
2021-01-27 16:28:01 -0800

    Solved Problems

--- codewars/iq_test.py
+++ codewars/iq_test.py
@@ -1,22 +1,7 @@
-def iq_test(numbers: str) -> int:
-    """Finds odd number out.
+def iq_test(numbers):
+    nums, parity = numbers.split(), {'evens': [], 'odds': []}
 
-    In a group of numbers, if every number but one is odd,
-    it'll return the odd number. Vice-versa for even numbers.
+    for i in nums:
+        parity['evens'].append(i) if int(i) % 2 == 0 else parity['odds'].append(i)
 
-    Args:
-        numbers: A string of numbers
-
-    Returns:
-        The odd or even number out."""
-    num_cast = list(map(int, numbers.split(' ')))
-    evens = [i for i in num_cast if i % 2 == 0]
-    odds = [i for i in num_cast if i % 2 != 0]
-    if len(evens) > len(odds):
-        for i, num in enumerate(num_cast, start=1):
-            if num == odds[0]:
-                return i
-    else:
-        for i, num in enumerate(num_cast, start=1):
-            if num == evens[0]:
-                return i
+    return nums.index(min((parity['evens'], parity['odds']), key=len)[0]) + 1
